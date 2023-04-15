35a44c1d422f6c2f5855f834fb23ff531511e2fe
JordanU <42947912+UJordan@users.noreply.github.com>
2023-04-15 15:18:20 -0700

    CORS

--- models/favoriteRouter.js
+++ models/favoriteRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('./cors');
 const authenticate = require('../authenticate');
 const Favorite = require('../models/favorite');
 
